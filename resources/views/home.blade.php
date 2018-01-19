97004164064e719d4ee1f6d3877acca1f661b75a
Ryuday <eliomayz@gmail.com>
2018-01-18 22:22:36 -0400

    navBar-Bootstrap4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.app')
 @section('title', "Usuarios")
-@section('subtitle', 'Listado de usuarios')
+@section('subtitle')
+  <div class="row justify-content-between">
+    <div class="col">
+      Listado de usuarios
+    </div>
+    <div class="col">
+      <a href="#">Registrar usuario</a>
+    </div>
+  </div>
+@endsection
 @section('content')
-  @if ("{{ Auth::user()->isAdmin }}")
-    <div class="">
-      {{ Auth::user()->name }}
+  @if (session('status'))
+    <div class="alert alert-success">
+      {{ session('status') }}
     </div>
   @endif
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <ul>
-          @forelse ( $users as $user )
-            <li>
-              {{ $user->name }}, ({{ $user->email }})
-              <a href="{{ route('users.show', ['id' => $user->id]) }}">Ver detalles</a>
-            </li>
-          @empty
-            <li>No hay usuarios registrados.</li>
-          @endforelse
-        </ul>
+  @if ( Auth::user()->is_admin === 1)
+    <ul>
+      @forelse ( $users as $user )
+        <li>
+          {{ $user->name }}, ({{ $user->email }})
+          <a href="{{ route('users.show', ['id' => $user->id]) }}">Ver detalles</a>
+        </li>
+      @empty
+        <li>No hay usuarios registrados.</li>
+      @endforelse
+    </ul>
+  @else
+    <a href="{{ route('users.show', ['id' => Auth::user()->id]) }}">Ver detalles</a>
+  @endif
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Bootstrap JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -16,62 +21,51 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Clubintec</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Clubintec') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Iniciar Sesion</a></li>
-                            <li><a href="{{ route('register') }}">Registrar</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Cerrar sesión
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+  <nav class="navbar navbar-expand-lg navbar-default bg-default navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <!-- Collapsed Hamburger -->
+        <a class="navbar-brand" href="{{ url('/') }}">Clubintec</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+            <span class="sr-only">Clubintec</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <!-- Right Side Of Navbar -->
+        <ul class="nav navbar-nav navbar-right">
+          <!-- Authentication Links -->
+          @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">Iniciar Sesion</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">Registrar</a>
+            </li>
+          @else
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->username }}
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+                  Cerrar Sesión
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+              </div>
+            </li>
+          @endguest
+        </ul>
+      </div>
+    </div>
+  </nav>
+      <div id="app">
         <div class="container">
           <div class="row">
             <div class="col-md-8 col-md-offset-2">
